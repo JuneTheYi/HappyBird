146f68a652df019f6de73ab1dddf25be667b76c7
June <junetheyi@gmail.com>
2022-04-20 22:39:14 -0400

    added controller, scenebuilder, login image

--- src/main/java/com/example/happybird/HelloController.java
+++ src/main/java/com/example/happybird/HelloController.java
@@ -2,13 +2,17 @@ package com.example.happybird;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private ImageView button;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    protected void userLogin() {
+        HelloApplication start = new HelloApplication();
+        start.changeScene("");
     }
 }
